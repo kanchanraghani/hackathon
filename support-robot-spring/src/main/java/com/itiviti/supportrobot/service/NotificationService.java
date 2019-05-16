ecb1c45930710016320d19fc5bcbfc0d0b6fb934
Daniel Filimon <daniel.filimon@itiviti.com>
2019-05-16 13:35:27 +0300

    Add backend request resolver

--- support-robot-spring/src/main/java/com/itiviti/supportrobot/service/NotificationService.java
+++ support-robot-spring/src/main/java/com/itiviti/supportrobot/service/NotificationService.java
@@ -1,24 +1,29 @@
 package com.itiviti.supportrobot.service;
 
+import java.io.File;
+import java.nio.file.Path;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import com.itiviti.supportrobot.domain.User;
 
 @Service
 public class NotificationService
 {
-    private JavaMailSender javaMailSender;
+    private JavaMailSender emailSender;
 
     @Autowired
     public NotificationService(JavaMailSender javaMailSender)
     {
-        this.javaMailSender = javaMailSender;
+        this.emailSender = javaMailSender;
     }
 
-    public void sendNotification(User user) throws MailException
+    public void sendNotification(User user)
     {
         // send email
         SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -27,6 +32,23 @@ public class NotificationService
         mailMessage.setSubject("Your Support Request");
         mailMessage.setText("Here's your request:...");
 
-        javaMailSender.send(mailMessage);
+        emailSender.send(mailMessage);
+    }
+
+    public void sendReplyWithAttachment(User user, Path path) throws MessagingException
+    {
+        // send email
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("support@itiviti.com");
+        helper.setTo(user.getEmailAddress());
+        helper.setSubject("Your Support Request");
+        helper.setText("Please find the requested information attached.");
+
+        FileSystemResource file = new FileSystemResource(path.toFile());
+        helper.addAttachment(file.getFilename(), file);
+
+        emailSender.send(message);
     }
 }
