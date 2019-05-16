2842735ea1875fca705c416451a0d870752714bb
Kaustubh Kulkarni <kaustubh.kulkarni@itiviti.com>
2019-05-17 02:35:44 +0530

    Add remaing files

--- support-robot-angular/src/app/app.module.ts
+++ support-robot-angular/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDatepickerModule, MatInputModule,MatNativeDateModule} from '@angular/material';
-import {ReactiveFormsModule} from '@angular/forms';
+import { MatDatepickerModule, MatInputModule, MatNativeDateModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { RequestFormComponent } from './request-form/request-form.component';
 
@@ -16,8 +17,8 @@ import { RequestFormComponent } from './request-form/request-form.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-	ConfirmationComponent,
-	RequestFormComponent
+	  ConfirmationComponent,
+	  RequestFormComponent
   ],
   imports: [
       BrowserModule,
@@ -25,7 +26,8 @@ import { RequestFormComponent } from './request-form/request-form.component';
       MatDatepickerModule, MatInputModule,MatNativeDateModule,
       FormsModule,
       ReactiveFormsModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
