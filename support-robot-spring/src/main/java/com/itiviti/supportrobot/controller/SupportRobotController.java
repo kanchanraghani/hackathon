9ee99db116d0f31317304931f14282c87e35e46b
shemanifisher <shemani@fisherits.com>
2019-05-17 00:14:09 +0100

    Added filter to disable CORS

--- support-robot-spring/src/main/java/com/itiviti/supportrobot/controller/SupportRobotController.java
+++ support-robot-spring/src/main/java/com/itiviti/supportrobot/controller/SupportRobotController.java
@@ -1,12 +1,18 @@
 package com.itiviti.supportrobot.controller;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import javax.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,18 +36,20 @@ public class SupportRobotController
 
     @RequestMapping(value = "/submitrequest", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
-    public String process(@RequestBody SupportRequest supportRequest)
+    public ResponseEntity process(@RequestBody SupportRequest supportRequest)
     {
         switch (supportRequest.getRequestType())
         {
             case 1:
-                return logsRequest(supportRequest);
+                 logsRequest(supportRequest);
             case 2:
-                return disconnectReasonRequest(supportRequest);
+                 disconnectReasonRequest(supportRequest);
             case 3:
-                return sessionDetailsRequest(supportRequest);
+                 sessionDetailsRequest(supportRequest);
         }
-        return "We could not process your request.";
+//        return "We could not process your request.";
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 
     private String logsRequest(SupportRequest supportRequest)
