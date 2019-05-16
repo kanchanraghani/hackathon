6e79f00f5aece38b0c43bebbd23c5a5ba37736f2
Nishant Jain <nishant.jain@itiviti.com>
2019-05-16 13:23:01 +0100

    Form updated

--- support-robot-angular/src/app/app.module.ts
+++ support-robot-angular/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule, MatInputModule,MatNativeDateModule} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
   ],
   imports: [
-    BrowserModule,
-	FormsModule
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatDatepickerModule, MatInputModule,MatNativeDateModule,
+      FormsModule,
+      ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
