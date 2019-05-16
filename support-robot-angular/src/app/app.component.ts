a1074f4dc49dd3416e2afa0176eb471bf2eb978c
Nishant Jain <jainnishant91@gmail.com>
2019-05-16 10:53:26 +0100

    Base Form

--- support-robot-angular/src/app/app.component.ts
+++ support-robot-angular/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  model: any = {};
   title = 'Support Bot';
+  
+    onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
+    }
 }
